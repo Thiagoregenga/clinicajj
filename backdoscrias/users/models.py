d722bc445fe5526b75985fe80d4361695e4c516d
Thiagoregenga <thiagomr3@al.insper.edu.br>
2023-08-11 06:50:00 -0300

    feito

--- backdoscrias/users/models.py
+++ backdoscrias/users/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password, check_password
 from django.db import models
 
 class CustomUser(models.Model):
@@ -6,4 +7,20 @@ class CustomUser(models.Model):
     phone = models.CharField(max_length=20)
     birthdate = models.DateField()
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=255) 
+    password = models.CharField(max_length=255)
+
+    def save(self, *args, **kwargs):
+        # If the password isn't hashed, hash it
+        if not self.password.startswith(('pbkdf2_sha256$', 'bcrypt')):
+            self.password = make_password(self.password)
+        super(CustomUser, self).save(*args, **kwargs)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+
+    class Meta:
+        verbose_name = 'Custom User'
+        verbose_name_plural = 'Custom Users'
+
+    def __str__(self):
+        return self.name
